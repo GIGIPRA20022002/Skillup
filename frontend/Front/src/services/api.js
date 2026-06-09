84f0ddec7f572386c53a2490ac05ea380e116063
Juan Jose Giraldo <juan.giraldo@etu.univ-poitiers.fr>
2026-06-09 14:47:39 -0500

    Ruta para traer los ejercicios del back al front

--- frontend/Front/src/services/api.js
+++ frontend/Front/src/services/api.js
@@ -1,7 +1,7 @@
 // Conexión REAL al backend de Python
 export const evaluateCode = async (code, module, studentId) => {
   try {
-    const response = await fetch('http://127.0.0.1:8000/analizar', {
+    const response = await fetch('https://skillup-wj4d.onrender.com/analizar', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -22,6 +22,24 @@ export const evaluateCode = async (code, module, studentId) => {
     
   } catch (error) {
     console.error("Error conectando con el backend:", error);
+    throw error;
+  }
+};
+
+// Función para obtener los ejercicios desde el backend
+export const getExercises = async () => {
+  try {
+    const response = await fetch('https://skillup-wj4d.onrender.com/ejercicios');
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+    
+  } catch (error) {
+    console.error("Error trayendo los ejercicios del backend:", error);
     throw error;
   }
 };
